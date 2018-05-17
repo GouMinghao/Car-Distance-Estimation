7eaca4a1d2a3552977765046899d8b72451b41d9
Minghao Gou <gmh2015@163.com>
2018-05-17 08:46:30 +0800

    modify generating video

--- Generating Video/genVideo.m
+++ Generating Video/genVideo.m
@@ -5,13 +5,16 @@ function genVideo(foldername)
     detector = vision.CascadeObjectDetector('../Integration/data/carDetector2.xml');
     myObj = VideoWriter(strcat('./',foldername,'/',foldername),'MPEG-4');%初始化一个avi文件
     myObj.FrameRate = 10;
+    videoDepth = VideoWriter(strcat('./',foldername,'/depth-',foldername),'MPEG-4');%初始化一个avi文件
+    videoDepth.FrameRate = 10;
     open(myObj);
+    open(videoDepth);
     for n = 2 : num
         filename = num2str(n);
-        I1 = imread(strcat('../Video/',foldername,'/L/',filename,'.jpg'));
-        I2 = imread(strcat('../Video/',foldername,'/R/',filename,'.jpg'));
+        I1 = imread(strcat('../Video/',foldername,'/R/',filename,'.jpg'));
+        I2 = imread(strcat('../Video/',foldername,'/L/',filename,'.jpg'));
         [J1,J2] = rectifyStereoImages(I1,I2,stereoParams,'OutputView','valid');
-        d = disparity(rgb2gray(J1),rgb2gray(J2),'BlockSize',9,'Method','SemiGlobal');
+        d = disparity(rgb2gray(J1),rgb2gray(J2),'BlockSize',15,'Method','SemiGlobal');
         [height,width] = size(d);
         d0 = zeros(height,width);
         for i = 1:height
@@ -24,43 +27,21 @@ function genVideo(foldername)
             end
         end
 
-        
-%         subplot(3,1,2)
-%         imshow(d0/50)
-%         subplot(3,1,1)
-%         imshow(J1)
-%         title('left')
-%         subplot(3,1,3)
-%         imshow(J2)
-%         title('right')
-
-        % figure(2)
-        % subplot(2,1,1)
-        % pcolor(5 * d0)
-        % colorbar
-        % view(0,-90)
-        % shading flat
 
         xyzPoints = reconstructScene(d,stereoParams);
         Z = xyzPoints(:,:,3)/1000;
-        % subplot(2,1,2)
-        % surf(Z)
-        % colorbar
-        % shading flat
-        % view(0,-90)
-
 
         img = J1;
         bbox = step(detector,img);
         detectedImg = img;
 
         [h,w] = size(bbox);
-        cars = cell(h);
-        distances = cell(h);
+        % cars = cell(1);
+        distances = cell(1);
         distance = zeros(h,1);
         for i = 1 : h
-            cars{i} = img(bbox(i,2):bbox(i,2) + bbox(i,4),bbox(i,1):bbox(i,1) + bbox(i,3));
-            distances{i} = Z(bbox(i,2):bbox(i,2) + bbox(i,4),bbox(i,1):bbox(i,1) + bbox(i,3));
+            % cars{i} = img(round(bbox(i,2) + 1/3* bbox(i,4)):round(bbox(i,2) + 2/3* bbox(i,4)),round(bbox(i,1) + 1/3* bbox(i,3)):round(bbox(i,1) + 2/3* bbox(i,3)),:);
+            distances{i} = Z(round(bbox(i,2) + 1/3* bbox(i,4)):round(bbox(i,2) + 2/3* bbox(i,4)),round(bbox(i,1) + 1/3* bbox(i,3)):round(bbox(i,1) + 2/3* bbox(i,3)));
             dis=distances{i};
             [hd,wd] = size(dis);
             s = 1;
@@ -74,15 +55,12 @@ function genVideo(foldername)
                 end
             end
             distance(i) = median(median(nonandis(1:s-1)));
-            % figure(i + 2)
-            % imshow(cars{i})
-            detectedImg = insertObjectAnnotation(detectedImg,'rectangle',bbox(i,:),strcat('Car ',num2str(i)));
-            detectedImg = insertObjectAnnotation(detectedImg,'rectangle',[1,20 * (i - 1) + 1,1,1],strcat(num2str(distance(i)),'m'));
+            detectedImg = insertObjectAnnotation(detectedImg,'rectangle',bbox(i,:),strcat(num2str(distance(i)),'m'));
         end
+        % save(strcat('./',foldername,'/',num2str(n)),'cars','distances')
 
-
-        imshow(detectedImg)
-        imwrite(detectedImg,strcat('./',foldername,'/',num2str(n-1),'.jpg'));
+        imshow(d0/15.0)
+        writeVideo(videoDepth,d0/(max(max(d0))));   
         writeVideo(myObj,detectedImg);
 
     end
